9b843a1f53598d795be74e71749a53cb1c945b0d
PanasenkoMaks <panasenko_maks@mail.ru>
2022-06-07 13:50:13 +0500

    Initial commit

---
+++ src/main/java/com/example/lesson2_12/Lesson212Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson2_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson212Application.class, args);
+    }
+
+}
